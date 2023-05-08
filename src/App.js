0de5482a50cb596b5b70e77db056832e9efedd28
Volha Ivanova <Volha.Ivanova@itechart-group.com>
2023-05-08 10:23:54 +0300

    created authentication page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.component";
-import SignUpForm from "./routes/sign-up-form/sign-up-form.component";
+import Authentication from "./routes/authentication/authentication";
 
 const Shop = () => {
   return <h1>I am a shop</h1>;
@@ -14,7 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
